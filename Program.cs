539ee3269b4d7fb7117cc94a3fb909f866952dcc
borisbvf <borisbvf@outlook.com>
2024-09-30 12:39:11 +0900

    Add calculation of time for search
    Adds duration time, refactors code.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿
-// The lengths of the snake cube puzzle
+﻿// The lengths of the snake cube puzzle
 int[] lengthValues = [3, 2, 3, 2, 2, 4, 2, 3, 2, 3, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 2, 3, 4, 2, 2, 2, 4, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 2];
 
 // The dimension of the cube
@@ -22,9 +21,13 @@ for (int x = 0; x < cubeSize; x++)
 List<Tuple<int, int, int>> path = new();
 
 state[0][0][0] = true;
+DateTime start = DateTime.Now;
 
 // Run a search
-bool result = FindSolution(	[0, 0, 0], 0, 1, 0,	path);
+bool result = FindSolution([0, 0, 0], 0, 1, 0);
+
+TimeSpan diff = DateTime.Now - start;
+Console.WriteLine($"Finding a solution took {diff.TotalSeconds} seconds.");
 
 // Show a solution
 foreach (Tuple<int, int, int> move in path)
@@ -48,77 +51,70 @@ foreach (Tuple<int, int, int> move in path)
 Console.ReadLine();
 
 // Recursive function with backtracking
-bool FindSolution(int[] start, int axis, int sign, int lengthIndex,	List<Tuple<int, int, int>> path)
+bool FindSolution(int[] start, int axis, int sign, int lengthIndex)
 {
-	int length = (lengthValues[lengthIndex] - 1) * sign;
+	int length = (lengthValues[lengthIndex] - 1);
 
-	int delta0 = axis == 0 ? length : 0;
-	int delta1 = axis == 1 ? length : 0;
-	int delta2 = axis == 2 ? length : 0;
-	int[] current = [start[0] + delta0, start[1] + delta1, start[2] + delta2];
+	int[] current = GetResultPoint(start, axis, sign, length);
 
 	// Check if the new point is out of the cube dimensions
 	if (current[axis] < 0 || current[axis] >= cubeSize)
-	{
 		return false;
-	}
 
 	// Check if the new point crosses earlier added
-	for (int i = 1; i <= Math.Abs(length); i++)
-	{
-		int d0 = axis == 0 ? i * sign : 0;
-		int d1 = axis == 1 ? i * sign : 0;
-		int d2 = axis == 2 ? i * sign : 0;
-		if (state[start[0] + d0][start[1] + d1][start[2] + d2])
-		{
-			return false;
-		}
-	}
+	if (!CheckState(start, axis, sign, length))
+		return false;
+
+	// Save the path
+	path.Add(new Tuple<int, int, int>(axis, sign, length + 1));
 
 	// Check if this is the last move
 	if (lengthIndex == lengthValues.Length - 1)
-	{
 		return true;
-	}
 
 	// Set state
-	for (int i = 1; i <= Math.Abs(length); i++)
-	{
-		int d0 = axis == 0 ? i * sign : 0;
-		int d1 = axis == 1 ? i * sign : 0;
-		int d2 = axis == 2 ? i * sign : 0;
-		state[start[0] + d0][start[1] + d1][start[2] + d2] = true;
-	}
-
-	// Save the path
-	path.Add(new Tuple<int, int, int>(axis, sign, Math.Abs(length) + 1));
+	SetState(start, axis, sign, length, true);
 
 	// Try recursive next ways from here
-	for (int i = 0; i <= 2; i++)
-	{
-		if (i != axis)
-		{
-			for (int j = -1; j <= 1; j += 2)
-			{
-				if (FindSolution(current, i, j, lengthIndex + 1, path))
-				{
+	for (int nextAxis = 0; nextAxis <= 2; nextAxis++)
+		if (nextAxis != axis)
+			for (int nextSign = -1; nextSign <= 1; nextSign += 2)
+				if (FindSolution(current, nextAxis, nextSign, lengthIndex + 1))
 					return true;
-				}
-			}
-		}
-	}
 
 	// If there is no way to the result, roll back the path
 	path.RemoveAt(path.Count - 1);
 
 	// Get state back
-	for (int i = 1; i <= Math.Abs(length); i++)
-	{
-		int d0 = axis == 0 ? i * sign : 0;
-		int d1 = axis == 1 ? i * sign : 0;
-		int d2 = axis == 2 ? i * sign : 0;
-		state[start[0] + d0][start[1] + d1][start[2] + d2] = false;
-	}
+	SetState(start, axis, sign, length, false);
 
 	return false;
+}
+
+int[] GetResultPoint(int[] start, int axis, int sign, int length)
+{
+	int x = axis == 0 ? length * sign : 0;
+	int y = axis == 1 ? length * sign : 0;
+	int z = axis == 2 ? length * sign : 0;
+	return [start[0] + x, start[1] + y, start[2] + z];
+}
+
+void SetState(int[] start, int axis, int sign, int length, bool value)
+{
+	for (int i = 1; i <= length; i++)
+	{
+		int[] point = GetResultPoint(start, axis, sign, i);
+		state[point[0]][point[1]][point[2]] = value;
+	}
+}
+
+bool CheckState(int[] start, int axis, int sign, int length)
+{
+	for (int i = 1; i <= length; i++)
+	{
+		int[] point = GetResultPoint(start, axis, sign, i);
+		if (state[point[0]][point[1]][point[2]])
+			return false;
+	}
+	return true;
 }
